8e41c1d4ce71561a636ca21fb03b26fab1647843
Abdul Rehman <mabdul6492@gmail.com>
2023-10-12 17:24:21 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.setName("RWOnly");
+//      java: name has private access in com.driver.RWOnly
+
+        System.out.println(obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    String getName(){
+        return this.name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
 }
